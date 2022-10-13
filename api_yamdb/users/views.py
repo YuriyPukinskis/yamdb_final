d4a47e614cfaab5e50f1c36388a7c204abf85b20
Yuriy Pukinskis <pukinskis@mail.ru>
2022-10-13 07:51:57 +0300

    isort v4

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,10 +1,10 @@
+from api.permissions import IsRoleAdmin
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from users.models import CustomUser
-from api.permissions import IsRoleAdmin
 
+from users.models import CustomUser
 
 from .serializers import UserSerializer
 
