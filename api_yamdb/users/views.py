9ad95eb5e573c0da19342f2b16fd91134a731672
Yuriy Pukinskis <pukinskis@mail.ru>
2022-10-13 07:54:45 +0300

    isort v5

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -3,7 +3,6 @@ from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
 from users.models import CustomUser
 
 from .serializers import UserSerializer
